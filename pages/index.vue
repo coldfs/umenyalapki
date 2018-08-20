20aa434745f0953edc2029f0c0c83c1b43908777
Sergey Ivanov <s.ivanov@office.ngs.ru>
2018-08-20 18:02:59 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Lapki - Randomizer
+      </h1>
+      <h2 class="subtitle">
+        я ничего не хочу решать, у меня лапки
+      </h2>
+      <app/>
+    </div>
+  </section>
+</template>
+
+<script>
+import App from '~/components/App.vue'
+
+export default {
+  components: {
+    App
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+/* display: flex; */  
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 50px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 21px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
